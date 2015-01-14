aef264269992a58527bd7b434a8549cb53e1c155
Tiffany Lu <tweilu@mit.edu>
2015-01-14 18:20:02 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -31,7 +31,7 @@
 }
 
 // put new methods here
-- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
     // this will get called every time the player touches the screen
     [character flap];
 }
